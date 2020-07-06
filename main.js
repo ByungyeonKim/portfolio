f1042adbac6d855281ff0fa4ca0001c96754f703
ByungyeonKim <viva_kby@naver.com>
2020-07-06 23:00:39 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -3,10 +3,22 @@
 // 최상단메뉴 스크롤 시 고정
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', ()=>{
-    if(window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+//navbar 메뉴 클릭 시 해당 섹션 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
