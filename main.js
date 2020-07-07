c854adcb25308aa0fbbd260440511999ba16c916
Byungyeon Kim <66554164+ByungyeonKim@users.noreply.github.com>
2020-07-07 18:03:03 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ document.addEventListener('scroll', () => {
   }
 });
 
+//공통 함수 분리
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
 //navbar 메뉴 클릭 시 해당 section 이동
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -28,15 +34,24 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
-//공통 함수 분리
-function scrollIntoView(selector) {
-  const scrollTo = document.querySelector(selector);
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
-}
-
-//스크롤 시 home contents 투명도 조절
+//스크롤 다운 시 home contents 투명도 조절
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+//스크롤 다운 시 arrow-up 투명도 조절
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 3) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// arrow-up button 클릭 시 home section 이동
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
 });
