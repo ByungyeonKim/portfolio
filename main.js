cecf80486c7d261a1dcc1c9ad38625cc8df4b2d0
ByungyeonKim <viva_kby@naver.com>
2020-07-19 01:31:37 +0900

    Fix mobile screen

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ document.addEventListener('scroll', () => {
   if (scrollY > 2700 && scrollY < 3200) {
     historyList.classList.add('down');
   }
+  console.log(scrollY);
 });
 
 //스크롤 다운 시 arrow-up 투명도 조절
