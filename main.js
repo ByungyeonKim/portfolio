1f6096ea16a6220041a1380f1c7b9b8fb2d2e116
ByungyeonKim <viva_kby@naver.com>
2021-03-16 13:56:21 +0900

    JS, HTML, CSS: work 마무리
    work section 마무리 history section 시작

--- main.js
+++ main.js
@@ -91,37 +91,6 @@ nextBtn.addEventListener('click', () => {
   moveSlide();
 });
 
-
-
-// const workBtnContainer = document.querySelector('.work__categories');
-// const projectContainer = document.querySelector('.work__projects');
-// const projects = document.querySelectorAll('.project');
-// workBtnContainer.addEventListener('click', (e) => {
-//   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-//   if (filter == null) {
-//     return;
-//   }
-//   //categoryBtn 클릭 시 효과 구현
-//   const active = document.querySelector('.category__btn.selected');
-//   active.classList.remove('selected');
-//   const target =
-//     // ? = BUTTON이 맞으면 e.target을 실행하고 아니면(:) e.target.parentNode를 실행 해줘
-//     e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-//   target.classList.add('selected');
-
-//   projectContainer.classList.add('anim-out');
-//   setTimeout(() => {
-//     projects.forEach((project) => {
-//       if (filter === '*' || filter === project.dataset.type) {
-//         project.classList.remove('invisible');
-//       } else {
-//         project.classList.add('invisible');
-//       }
-//     });
-//     projectContainer.classList.remove('anim-out');
-//   }, 300);
-// });
-
 // IntersectionObserver API
 const sectionIds = [
   '#home', 
