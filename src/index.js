be0fc3f01c31ba517784e7fb9467a048003af5b1
ByungyeonKim <viva_kby@naver.com>
2021-05-16 17:32:23 +0900

    contact: 이메일 복사하기 기능 추가

--- src/index.js
+++ src/index.js
@@ -90,3 +90,25 @@ function displayCards(cards) {
 loadProjectCards().then((cards) => {
   displayCards(cards);
 });
+
+// ClipboardJS
+const clipboard = new ClipboardJS('.copy-btn');
+const tooltip = document.querySelector('.tooltip');
+
+clipboard.on('success', (e) => {
+  tooltip.innerText = '복사 되었습니다!';
+  tooltip.classList.add('show-tooltip');
+  setTimeout(() => {
+    tooltip.classList.remove('show-tooltip');
+  }, 1500);
+  e.clearSelection();
+});
+
+clipboard.on('error', (e) => {
+  tooltip.innerText = '지원하지 않는 브라우저입니다.';
+  tooltip.classList.add('show-tooltip');
+  setTimeout(() => {
+    tooltip.classList.remove('show-tooltip');
+  }, 1500);
+  e.clearSelection();
+});
